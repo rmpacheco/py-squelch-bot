dfb97fbe541df8aac426dd85402f3bd5c086ea92
Roman M. Pacheco <rmpacheco@gmail.com>
2020-08-08 07:06:50 +0000

    bot can now start and end matches but still is useless

--- main2.py
+++ main2.py
@@ -2,18 +2,29 @@ import cherrypy
 
 @cherrypy.popargs('match_id')
 class Matches(object):
+    def __init__(self):
+        self.matches = {}
+
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def start(self, match_id):
-        j = cherrypy.request.json
-        return str( j["yourBotIndex"])
+        #return str( j["yourBotIndex"])
+        if not match_id in self.matches:
+            self.matches[match_id] = cherrypy.request.json
+        pass
 
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def end(self, match_id):
+        if match_id in self.matches:
+            del self.matches[match_id]
+        pass
 
 class Bot(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def info(self):
-         return {"name":"Rombotblah"}
+         return {"name":"Rombot"}
 
 class Root(object):
     pass
@@ -27,9 +38,6 @@ conf = {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': 8282,
     },
-    #'/': {
-    #    'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-    #},
 }
 
 cherrypy.quickstart(root, '/', conf)
