03d579fb842a6e44a59370d0a250eff3822a2a5d
Roman M. Pacheco <rmpacheco@gmail.com>
2020-08-11 16:19:27 +0000

    added logging to match start and end

--- main2.py
+++ main2.py
@@ -1,27 +1,29 @@
-import cherrypy
+import cherrypy as cp
 
-@cherrypy.popargs('match_id')
+@cp.popargs('match_id')
 class Matches(object):
     def __init__(self):
         self.matches = {}
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
+    @cp.expose
+    @cp.tools.json_in()
     def start(self, match_id):
         if not match_id in self.matches:
-            self.matches[match_id] = cherrypy.request.json
+            self.matches[match_id] = cp.request.json
+            cp.log("created new match: " + match_id)
         pass
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
+    @cp.expose
+    @cp.tools.json_in()
     def end(self, match_id):
         if match_id in self.matches:
             del self.matches[match_id]
+            cp.log("ended match: " + match_id)
         pass
 
 class Bot(object):
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
+    @cp.expose
+    @cp.tools.json_out()
     def info(self):
          return {"name":"Rombot"}
 
@@ -39,4 +41,4 @@ conf = {
     },
 }
 
-cherrypy.quickstart(root, '/', conf)
+cp.quickstart(root, '/', conf)
