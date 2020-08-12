63812787595186c1d681a47a63e8209c2df495ff
Roman M. Pacheco <rmpacheco@gmail.com>
2020-08-12 21:43:06 +0000

    game start call stubbed out and reachable

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import cherrypy as cp
 
+class Log(object):
+    def info(msg, category=None):
+        if category is None:
+            cp.log(msg)
+        else:
+            cp.log("{" + category + "} - " + msg)
+
+@cp.popargs('game_id')
+@cp.popargs('match_id')
+class Games(object):
+    def __init__(self):
+        self.games = {}
+
+    @cp.expose
+    @cp.tools.json_in()
+    def start(self, match_id, game_id):
+        if not game_id in self.games:
+            self.games[game_id] = cp.request.json
+            Log.info("started new game: " + game_id)
+        pass
+
 @cp.popargs('match_id')
 class Matches(object):
     def __init__(self):
@@ -10,7 +31,7 @@ class Matches(object):
     def start(self, match_id):
         if not match_id in self.matches:
             self.matches[match_id] = cp.request.json
-            cp.log("created new match: " + match_id)
+            Log.info("created new match: " + match_id)
         pass
 
     @cp.expose
@@ -18,7 +39,7 @@ class Matches(object):
     def end(self, match_id):
         if match_id in self.matches:
             del self.matches[match_id]
-            cp.log("ended match: " + match_id)
+            Log.info("ended match: " + match_id)
         pass
 
 class Bot(object):
@@ -32,6 +53,7 @@ class Root(object):
 
 root = Root()
 root.match = Matches()
+root.match.game = Games()
 root.bot = Bot()
 
 conf = {
